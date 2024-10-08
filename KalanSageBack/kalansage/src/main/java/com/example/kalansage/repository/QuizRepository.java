59cf0d0445bcfb4675dd07c4a7e47863a41bcb10
Kontere TIENOU <kontere13e@gmail.com>
2024-10-08 18:59:25 +0000

    Dashboarb presque ok

--- KalanSageBack/kalansage/src/main/java/com/example/kalansage/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kalansage.repository;
-
-import com.example.kalansage.model.userAction.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
